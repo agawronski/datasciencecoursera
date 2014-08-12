0f59ecfe8eeb88abd5bcee39d736ffb133eda6a6
agawronski <aidangawronski@gmail.com>
2014-08-11 21:24:00 -0400

    solutions to the first assignment

--- complete_2.R
+++ complete_2.R
@@ -1,3 +1,6 @@
+rm(list=ls())
+getwd()
+setwd("C:/Users/NC/data_science")
 
 complete <- function(directory, id = 1:332){
   files<-list.files("specdata", full.names=TRUE)
@@ -7,8 +10,11 @@ complete <- function(directory, id = 1:332){
   }
   final_table<-data.frame() #empty
   for (i in id){
-    final_table<-rbind(final_table, c(i,sum(complete.cases(all_data[all_data$ID==i,]))))
+    final_table<-rbind(c(i,sum(complete.cases(all_data[all_data$ID==i,]))))
   }
   colnames(final_table)<-c("id","nobs")
   print(final_table)
+  class(final_table)
 }
+  
+
--- complete_original.R
+++ complete_original.R
@@ -2,6 +2,7 @@ rm(list=ls())
 getwd()
 setwd("C:/Users/NC/data_science")
 
+id<-30:25
 complete <- function(directory, id = 1:332){
   files<-list.files("specdata", full.names=TRUE) #character vec of filenames in specdata
   all_data<-data.frame() #empty frame
@@ -11,16 +12,8 @@ complete <- function(directory, id = 1:332){
   cc<-all_data[complete.cases(all_data),] #subset all complete cases
   cc_vec<-cc[,"ID"] #remove only the character vector of id numbers
   almost<-as.data.frame(table(cc_vec)) #tabulate and coerce to a df
-  colnames(almost)<-c("num","nobs") #change names
-  length(cc_vec[cc_vec==30])
-  done<-data.frame()
-  for(i in id){
-    done<-rbind(data.frame(i,length(cc_vec[cc_vec==i])))
-  }
-  colnames(done)<-c("num","nobs") #change names
-  print(done)
-  print(class(done))
-  print(names(done))
-  print(dim(done))
+  colnames(almost)<-c("id","nobs") #change names
+  names(almost)
+  print(almost)
 }
 
