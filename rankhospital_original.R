f8f570e80ced800c80d242d427b30a4fa1cbb7ee
agawronski <aidangawronski@gmail.com>
2014-08-20 08:43:48 -0400

    change name rankhospital

--- rankhospital_original.R
+++ rankhospital_original.R
@@ -1,7 +1,7 @@
 ## Rank hospitals by outcome in a state
 
 ## write a function called "rankhospital"
-
+ 
 ## state (eg. "NY"), outcome (eg. "pneumonia"), num (eg. "2")
 ## rankhospital reads "outcome-of-care-measures.csv"...
 ## returns a character vector with the hospital specified by num
@@ -10,15 +10,15 @@
 ## num > hospitals in state returns NA
 ## ties settled by alphabetical hospital name
 
-rankhospital <- function(state, outcome, num="best") {
-
-  #read outcome data
+rankhospital <- function(state, outcome, num="best"){
+  
+  ##Read outcome data
   data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
   data <- data[,c(2,7,11,17,23)]
   data[,c(3,4,5)] <- sapply(data[,c(3,4,5)], as.numeric)
   names(data) <- c("hospitals","states","heart attack","heart failure","pneumonia")
-
-  #check that state and outcome are vaild
+  
+  ##check that state and outcome are valid
   states <- data[,2]
   outcomes <- c("heart attack", "heart failure", "pneumonia")
   if(!(state%in%states)){stop("invalid state")}
@@ -26,21 +26,34 @@ rankhospital <- function(state, outcome, num="best") {
   
   #best and worst
   if (num == "best") {num <- 1}
-  if (num == "worst") {num <- length(na.omit(data[data$states==state & data[,outcome]!="NA", outcome]))}
-  if (num > length(na.omit(data[data$states==state & data[,outcome]!="NA", outcome]))) {num <- return(NA)}
+  if (num == "worst") {num <- length(data[states==state & data[,outcome]!="NA",])}
+  if (num > length(data[states==state & data[,outcome]!="NA",])) {num <- return(NA)}
   
-  #return first ordered value
-  if (outcome=="heart attack") {
-    sort <- data[order(data[,3], data[,1]), c(1,2,3)]
-    slice <- sort[sort[,2]==state,1]
+  #order by alphabet
+  data <- data[order(data[,1]),]
+  
+  ##return hospital name in that state with the given rank
+  ##30-day mort rate
+  if(outcome=="heart attack"){
+    odata <- data[order(data[,3]),]
+    sodata <- odata[odata[,2]==state,c(1,3)]
+    n <- nrow(sodata)
+    rank <- cbind(sodata,1:n)
+    match <- rank[rank[,3]==num,1]
   }
   else if (outcome=="heart failure") {
-    sort <- data[order(data[,4], data[,1]), c(1,2,4)]
-    slice <- sort[sort[,2]==state,1]
+    odata <- data[order(data[,4]),]
+    sodata <- odata[odata[,2]==state,c(1,3)]
+    n <- nrow(sodata)
+    rank <- cbind(sodata,1:n)
+    match <- rank[rank[,3]==num,1]
   }
   else {
-    sort <- data[order(data[,5], data[,1]), c(1,2,5)]
-    slice <- sort[sort[,2]==state,]
+    odata <- data[order(data[,5]),]
+    sodata <- odata[odata[,2]==state,c(1,3)]
+    n <- nrow(sodata)
+    rank <- cbind(sodata,1:n)
+    match <- rank[rank[,3]==num,1]
   }
-  slice[num]
+  match
 }
