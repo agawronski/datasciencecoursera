c686f2aba21aa9701510eff9e0d7104beaeb5b04
agawronski <aidangawronski@gmail.com>
2014-08-25 19:46:56 -0400

    rankall2 included

--- rankall2.R
+++ rankall2.R
@@ -28,12 +28,13 @@ rankall <- function (outcome, num = "best") {
     final_list <- lapply(worst, "[", 1,c(1,2),drop=TRUE) #make a df with the first entry on each list
   }
   else if(is.numeric(num)) {
-    best <- lapply(state_list, function(x) x[order(x[,"d"]),]) #ordering ascending
-    final_list <- lapply(worst, "[", num,c(1,2),drop=TRUE) #make a df with entry = num
+    order <- lapply(state_list, function(x) x[order(x[,"d"]),]) #ordering ascending
+    final_list <- lapply(order, "[", num,c(1,2),drop=TRUE) #make a df with entry = num
   }
   else {
     stop("invalid num")
   }
   final_df <- as.data.frame(do.call("rbind", final_list))
   final_df
+  
 }
